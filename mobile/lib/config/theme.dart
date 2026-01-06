c60019cb17b0f2ef92821ae779ce5e6adb4e12d9
Marcelofury <marcelofury13579@gmail.com>
2026-01-06 11:44:34 +0300

    updated color

--- mobile/lib/config/theme.dart
+++ mobile/lib/config/theme.dart
@@ -15,6 +15,7 @@ class AppColors {
   // Neutral Colors
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
+  static const Color grey50 = Color(0xFFFAFAFA);
   static const Color grey100 = Color(0xFFF5F5F5);
   static const Color grey200 = Color(0xFFEEEEEE);
   static const Color grey300 = Color(0xFFE0E0E0);
