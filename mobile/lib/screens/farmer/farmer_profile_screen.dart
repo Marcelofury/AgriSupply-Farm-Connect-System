e9a700f87f9614a3fd173046f79a3d0b72914f4d
Marcelofury <marcelofury13579@gmail.com>
2026-03-10 13:25:55 +0300

    updated districts in the locations

--- mobile/lib/screens/farmer/farmer_profile_screen.dart
+++ mobile/lib/screens/farmer/farmer_profile_screen.dart
@@ -39,10 +39,35 @@ class _FarmerProfileScreenState extends State<FarmerProfileScreen> {
 
   final List<String> _regions = ['Central', 'Eastern', 'Northern', 'Western'];
   final Map<String, List<String>> _districts = {
-    'Central': ['Kampala', 'Wakiso', 'Mukono', 'Mpigi', 'Luwero', 'Masaka'],
-    'Eastern': ['Jinja', 'Mbale', 'Soroti', 'Tororo', 'Iganga', 'Busia'],
-    'Northern': ['Gulu', 'Lira', 'Arua', 'Kitgum', 'Moroto', 'Pader'],
-    'Western': ['Mbarara', 'Fort Portal', 'Kabale', 'Kasese', 'Hoima', 'Masindi'],
+    'Central': [
+      'Buikwe', 'Bukomansimbi', 'Butambala', 'Buvuma', 'Gomba', 'Kalangala', 
+      'Kalungi', 'Kampala', 'Kayunga', 'Kiboga', 'Kyankwanzi', 'Luwero', 
+      'Lwengo', 'Lyantonde', 'Masaka', 'Mityana', 'Mpigi', 'Mubende', 
+      'Mukono', 'Nakaseke', 'Nakasongola', 'Rakai', 'Sembabule', 'Wakiso'
+    ],
+    'Eastern': [
+      'Amuria', 'Budaka', 'Bududa', 'Bugiri', 'Bugweri', 'Bukwa', 'Bulambuli', 
+      'Busia', 'Butaleja', 'Butebo', 'Buyende', 'Iganga', 'Jinja', 'Kaberamaido', 
+      'Kalaki', 'Kaliro', 'Kamuli', 'Kapchorwa', 'Kapelebyong', 'Katakwi', 
+      'Kibuku', 'Kumi', 'Kween', 'Luuka', 'Manafwa', 'Mayuge', 'Mbale', 
+      'Namayingo', 'Namisindwa', 'Namutumba', 'Ngora', 'Pallisa', 'Serere', 
+      'Sironko', 'Soroti', 'Tororo'
+    ],
+    'Northern': [
+      'Abim', 'Adjumani', 'Agago', 'Alebtong', 'Amudat', 'Amuru', 'Apac', 
+      'Arua', 'Dokolo', 'Gulu', 'Kaabong', 'Kitgum', 'Koboko', 'Kole', 
+      'Kotido', 'Lamwo', 'Lira', 'Maracha', 'Moroto', 'Moyo', 'Nabilatuk', 
+      'Napak', 'Nebbi', 'Ngora', 'Nwoya', 'Obongi', 'Omoro', 'Otuke', 
+      'Oyam', 'Pader', 'Pakwach', 'Yumbe', 'Zombo'
+    ],
+    'Western': [
+      'Buhweju', 'Buliisa', 'Bundibugyo', 'Bunyangabu', 'Bushenyi', 'Butobo', 
+      'Hoima', 'Ibanda', 'Isingiro', 'Kabale', 'Kabarole', 'Kagadi', 'Kakumiro', 
+      'Kamwenge', 'Kanungu', 'Kasese', 'Kibaale', 'Kikuube', 'Kiruhura', 
+      'Kiryandongo', 'Kisoro', 'Kitagwenda', 'Kyegegwa', 'Kyenjojo', 'Masindi', 
+      'Mbarara', 'Mitooma', 'Ntoroko', 'Ntungamo', 'Rubanda', 'Rubirizi', 
+      'Rukiga', 'Rukungiri', 'Rwampara', 'Sheema'
+    ],
   };
 
   @override
