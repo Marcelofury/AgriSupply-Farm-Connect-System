c41ad7086ab8fcd5f174838c7d748629f3406ec6
Marcelofury <marcelofury13579@gmail.com>
2026-05-19 16:52:17 +0300

    Normalize import ordering in farmer analytics

--- mobile/lib/screens/farmer/farmer_analytics_screen.dart
+++ mobile/lib/screens/farmer/farmer_analytics_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../config/theme.dart';
